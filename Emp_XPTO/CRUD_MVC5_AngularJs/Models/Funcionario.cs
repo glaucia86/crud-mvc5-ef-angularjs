81abbe4844909b3aebf5c70b23fd370639d0d950
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-01-01 16:45:32 -0200

    Ajuste de linhas no arquivo 'Funcionário.cs'

--- Emp_XPTO/CRUD_MVC5_AngularJs/Models/Funcionario.cs
+++ Emp_XPTO/CRUD_MVC5_AngularJs/Models/Funcionario.cs
@@ -15,9 +15,13 @@ namespace CRUD_MVC5_AngularJs.Models
     public partial class Funcionario
     {
         public int FuncionarioId { get; set; }
+
         public string Nome { get; set; }
+
         public string Departamento { get; set; }
+
         public string Cargo { get; set; }
+
         public string Email { get; set; }
     }
 }
